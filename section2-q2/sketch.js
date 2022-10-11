4615a81bee1b4fe8c2f69a02f9b7611dba96efe7
eari0107 <2010561h@stu.kobe-u.ac.jp>
2022-10-11 14:54:39 +0900

    change section2-q1

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,9 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      console.log(i,j);
+      stroke(0,0,255);
+      rect (i * 20, j * 20, 20, 20);
     }
   }
 }
